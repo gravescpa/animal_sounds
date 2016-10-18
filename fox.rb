64d5b823685e29868ec26d2bc09f6394f41250d7
Susan Graves <susan@susangravescpa.com>
2016-10-18 13:12:35 -0400

    added name & sound for dog & fox

---
+++ fox.rb
@@ -0,0 +1,9 @@
+class Fox
+    attr_reader :name, :sound
+    
+    def initialize(name)
+        @name = name
+        @sound = "screech"
+    end
+
+end
--- test_animal_sounds.rb
+++ test_animal_sounds.rb
@@ -4,7 +4,7 @@ require_relative 'cat.rb'
 require_relative 'fox.rb'
 
 class TestAnimalSounds < Minitest::Test
-	
+
 	def test_cat_name
 		animal = Cat.new("Stanley")
 		assert_equal("Stanley", animal.name)
@@ -13,5 +13,25 @@ class TestAnimalSounds < Minitest::Test
 	def test_cat_sound
 		animal = Cat.new("Stanley")
 		assert_equal("meow", animal.sound)
-		end	
+	end	
+
+	def test_dog_name
+		animal = Dog.new("Winston")
+		assert_equal("Winston", animal.name)
+	end	
+
+	def test_dog_sound
+		animal = Dog.new("Winston")
+		assert_equal("bark", animal.sound)
+	end	
+
+	def test_fox_name
+		animal = Fox.new("Foxy")
+		assert_equal("Foxy", animal.name)
+	end	
+
+	def test_fox_sound
+		animal = Fox.new("Foxy")
+		assert_equal("screech", animal.sound)
+	end	
 end
